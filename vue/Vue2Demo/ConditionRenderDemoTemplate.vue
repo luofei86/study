ed51ff8c7d0262483fc8e29ad223fd79a3f7fe04
luofei <luofei86@gmail.com>
2022-06-30 21:10:28 +0800

    add vue demo

---
+++ vue/Vue2Demo/ConditionRenderDemoTemplate.vue
@@ -0,0 +1,20 @@
+<template v-if='loginType ==== "userName"'>
+	<label>Username</label>
+	<input placeholder="Enter your username" key="username-input">
+</template>
+<template v-else>
+	<label>Email</label>
+	<input placeholder="Enter your email" key="email">
+</template>
+
+<script>
+	export default{
+		name: 'crdt',
+		props: {
+			loginType: String
+		}
+	}
+</script>
+
+<style>
+</style>
